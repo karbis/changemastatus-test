50148ae3cafdfb772398c896873ae8d1da8bb3fe
karbis <102873508+karbis@users.noreply.github.com>
2023-04-27 18:18:09 +0300

    asdfasdfasdf

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def read_item(data: Item):
         dicti = {"custom_status":{"text":data.status,"emoji_name":data.emoji}}
     response = requests.patch("https://discord.com/api/v9/users/@me/settings", headers={"authorization": T,"content-type": "application/json"}, data=json.dumps(dicti))
     if response.headers["X-RateLimit-Remaining"] and response.headers["X-RateLimit-Remaining"] == 1 {
-        thread = threading.Thread(target=doRateLimit, args=(response.headers,))
+        thread = threading.Thread(target=doRateLimit, args=(response["headers"],))
         thread.start()
     }
     return 200
